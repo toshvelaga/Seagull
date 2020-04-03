440ada81fd35574433f103a37e00ebc433cc9f87
Tosh Velaga <toshvelaga@gmail.com>
2020-04-02 19:50:45 -0700

    added material ui, folder structure, and signin pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-import Home from './containers/Home/Home'
+import Home from './business/containers/Home/Home'
+import SignIn from './business/containers/SignIn/SignIn'
+import SignUp from './business/containers/SignUp/SignUp'
 
 import {
   BrowserRouter as Router,
@@ -13,8 +15,16 @@ function App() {
   return (
     <div className="App">
       <Router>
-          <Route path='/Home'>
+          <Route path='/home'>
             <Home />
+          </Route>
+
+          <Route path='/signin'>
+            <SignIn />
+          </Route>
+          
+          <Route path='/signup'>
+            <SignUp />
           </Route>
       </Router>
     </div>
